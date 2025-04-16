42c9c2234096f33e7e4a71554843c77162270fac
MeatChips <84376178+MeatChips@users.noreply.github.com>
2025-04-16 14:28:44 +0200

    Prototype Stuff
    - Camel Puzzle
    - Player Update

---
+++ Assets/Scripts/Puzzles/CamelPuzzle/CamelPuzzle.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CamelPuzzle : MonoBehaviour
+{
+    [SerializeField] private GameObject camelBump;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            camelBump.transform.localScale += new Vector3(0, 0, 50);
+            Destroy(other.gameObject);
+        }
+    }
+}
