90621477650124b81d5592e7da82a41e0f992ea5
Bomduif <Dennisbouma07@gmail.com>
2025-05-19 17:05:58 +0200

    particle
    - added grass particle

--- Assets/Scripts/particle stop.cs
+++ Assets/Scripts/particle stop.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class particlestop : MonoBehaviour
 {
+    [SerializeField] private GameObject particle;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +12,9 @@ public class particlestop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if ()
+        {
+            ParticleSystem.Stop();
+        }
     }
 }
