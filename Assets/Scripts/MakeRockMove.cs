4b91bda212219f85ce2eebe678c68e911651a404
Bomduif <Dennisbouma07@gmail.com>
2025-05-21 18:37:09 +0200

    attempt at making rock movable
    an attempt at making the rock moveable

---
+++ Assets/Scripts/MakeRockMove.cs
@@ -0,0 +1,43 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Rendering;
+
+public class MakeRockMove : MonoBehaviour
+{
+
+    public bool PowerActive;
+    public Rigidbody rb;
+    private void Start()
+    {
+        //get the rigid body and make sure every setting is set to default
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+        PowerActive = false;
+    }
+
+    private void Update()
+    {//if the power is active (aka the previous event has been completed make the rock movable (by setting kinematic to false)
+        if (PowerActive == true)
+        {
+            rb.WakeUp();
+            rb.isKinematic = false;
+           
+        }
+    }
+    public void CamelPowerUse()
+    {
+        //when the event is recieved set power active to true
+        PowerActive = true;
+      
+        Debug.Log("Used");
+    }
+    
+    //public void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.G))
+     //   {
+    //        rb.isKinematic = true ;
+     //   }
+    //}
+}
--- Assets/Scripts/Scanning.cs
+++ Assets/Scripts/Scanning.cs
@@ -1,35 +1,39 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 
 public class Scanning : MonoBehaviour
 {
     public bool CamelPowerUnlocked;
-    public bool CamelPowerSelected;
-
+    public bool CamelPowerSelectFake;
+    [SerializeField] private UnityEvent CamelPowerSelected;
     
     public float ScanRange;
     public GameObject Object1;
     public GameObject Object2;
 
-
+    private void Start()
+    {
+        CamelPowerUnlocked = false;
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(Object1.transform.position, Object2.transform.position) < ScanRange) //&& Input.GetKeyDown(KeyCode.L))
+        //if the distance between the player and the scancube is less than scanrange and L is pressed unlock camel power
+        if (Vector3.Distance(Object1.transform.position, Object2.transform.position) < ScanRange && Input.GetKeyDown(KeyCode.L))
         {
+            CamelPowerUnlocked= true;
             Debug.Log("unlocked");
         }
-        else
+        
+      //if the camel power has been unlocked and R is pressed, select the camelpower. causing the rock to be able to be moved
+        if (CamelPowerUnlocked == true && Input.GetKeyDown(KeyCode.R))
+
         {
-            Debug.Log("locked");
+           
+            CamelPowerSelected.Invoke();
+         
         }
-        //if (CamelPowerUnlocked == false)
-        //{
-          //  Debug.Log("not unlocked");
-        //}
-          //  else //(CamelPowerUnlocked == true)
-        //{
-            
-        //}
+       
     }
 }
