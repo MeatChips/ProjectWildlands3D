14945026ac3edd808f4cf052d2ca9af177673baf
Bamnus <134952401+Bamnus@users.noreply.github.com>
2025-05-28 10:12:18 +0200

    Reset button in pause menu

---
+++ Assets/Scripts/PauseMenu/ResetScene.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reset : MonoBehaviour
+{
+    public void ResetRequest()
+    {
+        string currentScene = SceneManager.GetActiveScene().name; // Get the current active scene
+        SceneManager.LoadScene(currentScene);
+    }
+}
