543ed5efd99b006c32d7e532d8c34b61b0a05509
Bamnus <134952401+Bamnus@users.noreply.github.com>
2025-06-02 14:29:15 +0200

    idk bro this shit weird
    treid getting start and select to open and close the menu

--- Assets/Scripts/PauseMenu/PauseSystem.cs
+++ Assets/Scripts/PauseMenu/PauseSystem.cs
@@ -1,18 +1,15 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PauseSystem : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu = null;
 
     bool isPaused;
-
-    void Update()
+    public void PauseGame(InputAction.CallbackContext context)
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            isPaused = !isPaused;
-            Time.timeScale = isPaused ? 0 : 1;
-            pauseMenu.SetActive(isPaused);
-        }
+        isPaused = !isPaused;
+        Time.timeScale = isPaused ? 0 : 1;
+        pauseMenu.SetActive(isPaused);
     }
 }
