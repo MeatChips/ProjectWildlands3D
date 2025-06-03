afb3e077fe88d2ef36ef55a8722400e9f33db5c9
Bomduif <Dennisbouma07@gmail.com>
2025-06-03 12:43:50 +0200

    spawn ship part
    -added spawning the ship part when the prairie dogs are close together

--- Assets/Scripts/Prairiedogpuzzle.cs
+++ Assets/Scripts/Prairiedogpuzzle.cs
@@ -5,6 +5,8 @@ public class Prairiedogpuzzle : MonoBehaviour
     [SerializeField] private float Proximity;
     [SerializeField] private GameObject Object1;
     [SerializeField] private GameObject Object2;
+    [SerializeField] private GameObject Object3;
+    public bool iscreated;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,8 +16,10 @@ public class Prairiedogpuzzle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(Object1.transform.position, Object2.transform.position) < Proximity)
+        if (Vector3.Distance(Object1.transform.position, Object2.transform.position) < Proximity && !iscreated) 
         {
+            Instantiate(Object3,transform.position, Quaternion.identity);
+            iscreated = true;
             Debug.Log("doine");
         }
     }
