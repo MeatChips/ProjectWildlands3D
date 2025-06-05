0fec1db21c398b6d06e2694e77c60ff3b4246238
smiling897 <lillyburkhardt@gmx.de>
2025-06-05 14:56:04 +0200

    added a comment

--- Assets/Scripts/Intro outro/Intro 2.cs
+++ Assets/Scripts/Intro outro/Intro 2.cs
@@ -5,6 +5,6 @@ public class Intro2 : MonoBehaviour
 {
     public void OnOutroComplete()
     {
-        SceneManager.LoadScene("Intro Comic 2"); // after intro scene 2 got to the level
+        SceneManager.LoadScene("Intro Comic 2"); // after intro scene 1 go to intro scene 2
     }
 }
