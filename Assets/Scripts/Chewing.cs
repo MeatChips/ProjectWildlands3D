46b800f248513f2fef857913fcfa31ef13195a24
Bomduif <Dennisbouma07@gmail.com>
2025-06-05 15:02:05 +0200

    wall particle effect
    - added a particle effect for a wall break

--- Assets/Scripts/Chewing.cs
+++ Assets/Scripts/Chewing.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Chewing : MonoBehaviour
 {
+    [SerializeField] GameObject Object1;
+    [SerializeField] GameObject Object2;
+
+    public float ChewRange;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +16,10 @@ public class Chewing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(Object1.transform.position, Object2.transform.position) < ChewRange && Input.GetKeyDown(KeyCode.R))
+        {
+            Destroy(gameObject);
+            Debug.Log("destroyed");
+        }
     }
 }
