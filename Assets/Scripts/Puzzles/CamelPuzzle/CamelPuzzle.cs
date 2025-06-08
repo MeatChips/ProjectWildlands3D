3a72b2c2759a2879e0680d32122a1f19c4727022
MeatChips <84376178+MeatChips@users.noreply.github.com>
2025-06-08 12:06:34 +0200

    Camel Update
    - Added particles
    - Added UI
    - Camel UI bug fixed

--- Assets/Scripts/Puzzles/CamelPuzzle/CamelPuzzle.cs
+++ Assets/Scripts/Puzzles/CamelPuzzle/CamelPuzzle.cs
@@ -5,16 +5,13 @@ public class CamelPuzzle : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private GameObject walkColliderOne;
     [SerializeField] private GameObject walkColliderTwo;
+    [SerializeField] private GameObject ChewParticle;
+    [SerializeField] private GameObject uiCanvas;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
     }
-
-    [SerializeField] private GameObject camelBump;
-    [SerializeField] private GameObject ChewParticle;
-    [SerializeField] private GameObject uiCanvas;
-    [SerializeField] private MonoBehaviour uiCamel;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("PickUp") && !other.gameObject.name.Contains("SpaceShip"))
@@ -26,11 +23,7 @@ public class CamelPuzzle : MonoBehaviour
 
             ChewParticle.SetActive(true); // Activate the chewing particle effect
 
-            uiCamel.enabled = false; // Disable the UI camel script to prevent further interaction
-
-            uiCanvas.SetActive(false); // Hide the UI canvas when the camel eats the object
-
-
+            uiCanvas.gameObject.SetActive(false); // Hide the UI canvas when the camel eats the object
         }
     }
 }
